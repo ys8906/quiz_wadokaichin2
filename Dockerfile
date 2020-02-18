470ea212bad7f243e3e6b726c6e791b195471840
Yoshito Sato <yoshitosato@YoshitonoMacBook-Pro.local>
2020-02-18 13:46:51 +0900

    クイズ作成前に確認してから、対話で保存するか選択できるように設定

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,8 @@ ENV LANG C.UTF-8
 RUN apt-get update -qq && \
     apt-get install -y build-essential \ 
                        libpq-dev \        
-                       nodejs           
+                       nodejs \
+                       default-mysql-client         
 
 # Install Yarn 
 RUN apt-get update && apt-get install -y curl apt-transport-https wget && \
--- app/services/generate_quiz_service.rb
+++ app/services/generate_quiz_service.rb
@@ -5,22 +5,44 @@ class GenerateQuizService
 
   class << self
 
-    # TODO: need to prevent quiz from overlapping (though unlikely to happen)
+    # TODO: need to prevent overlapping quiz (though unlikely to happen)
+    # OPTIMIZE
+    # FIXME
 
     def generate_wadokaichin(kanji)         # GenerateQuizService.generate_wadokaichin()
-      jukugo_left = Jukugo.where("name like ?", "#{kanji}%").sample(2)
-      jukugo_right = Jukugo.where("name like ?", "%#{kanji}").sample(2)
-      if jukugo_left.count == 2 && jukugo_right.count == 2
-        QuizWadokaichin.create(answer: kanji,
-                                jukugo1_name: jukugo_left[0].name,
-                                jukugo2_name: jukugo_left[1].name,
-                                jukugo3_name: jukugo_right[0].name,
-                                jukugo4_name: jukugo_right[1].name)
-        QuizWadokaichin.last.build_quiz_wadokaichin_jukugo\   # line-break
-          .update(jukugo1_id: jukugo_left[0].id, jukugo2_id: jukugo_left[1].id,
-                  jukugo3_id: jukugo_right[0].id, jukugo4_id: jukugo_right[1].id)
-      else
-        p "n/a"
+      loop do
+        jukugo_left = Jukugo.where("name like ?", "#{kanji}%").sample(2)
+        jukugo_right = Jukugo.where("name like ?", "%#{kanji}").sample(2)
+        if jukugo_left.count == 2 && jukugo_right.count == 2
+          p [kanji, jukugo_left[0].name, jukugo_left[1].name, jukugo_right[1].name]
+          p "これで作成しますか？[y/n]"
+          response = gets
+          case response
+          when /^[yY]/
+            QuizWadokaichin.create(answer: kanji,
+                                    jukugo1_name: jukugo_left[0].name,
+                                    jukugo2_name: jukugo_left[1].name,
+                                    jukugo3_name: jukugo_right[0].name,
+                                    jukugo4_name: jukugo_right[1].name)
+            QuizWadokaichin.last.build_quiz_wadokaichin_jukugo\
+              .update(jukugo1_id: jukugo_left[0].id, jukugo2_id: jukugo_left[1].id,
+                      jukugo3_id: jukugo_right[0].id, jukugo4_id: jukugo_right[1].id)
+            return p "作成成功: #{[kanji, jukugo_left[0].name, jukugo_left[1].name, jukugo_right[1].name]}"
+          when /^[nN]/, /^$/
+            p "作り直しますか？[y/n]"
+            response = gets
+            case response
+            when /^[yY]/
+              p "再生成"
+              redo
+            when /^[nN]/, /^$/
+              p "n/a"
+              return
+            end
+          end
+        else
+          p "n/a"
+        end
       end
     end
 
@@ -30,15 +52,32 @@ class GenerateQuizService
         jukugo_left = Jukugo.where("name like ?", "#{kanji.character}%").sample(2)
         jukugo_right = Jukugo.where("name like ?", "%#{kanji.character}").sample(2)
         if jukugo_left.count == 2 && jukugo_right.count == 2
-          QuizWadokaichin.create(answer: kanji,
-                                  jukugo1_name: jukugo_left[0].name,
-                                  jukugo2_name: jukugo_left[1].name,
-                                  jukugo3_name: jukugo_right[0].name,
-                                  jukugo4_name: jukugo_right[1].name)
-          QuizWadokaichin.last.build_quiz_wadokaichin_jukugo\
-            .update(jukugo1_id: jukugo_left[0].id, jukugo2_id: jukugo_left[1].id,
-                    jukugo3_id: jukugo_right[0].id, jukugo4_id: jukugo_right[1].id)
-          return
+          p [kanji.character, jukugo_left[0].name, jukugo_left[1].name, jukugo_right[1].name]
+          p "これで作成しますか？[y/n]"
+          response = gets
+          case response
+          when /^[yY]/
+            QuizWadokaichin.create(answer: kanji.character,
+                                    jukugo1_name: jukugo_left[0].name,
+                                    jukugo2_name: jukugo_left[1].name,
+                                    jukugo3_name: jukugo_right[0].name,
+                                    jukugo4_name: jukugo_right[1].name)
+            QuizWadokaichin.last.build_quiz_wadokaichin_jukugo\
+              .update(jukugo1_id: jukugo_left[0].id, jukugo2_id: jukugo_left[1].id,
+                      jukugo3_id: jukugo_right[0].id, jukugo4_id: jukugo_right[1].id)
+            return p "作成成功: #{[kanji.character, jukugo_left[0].name, jukugo_left[1].name, jukugo_right[1].name]}"
+          when /^[nN]/, /^$/
+            p "作り直しますか？[y/n]"
+            response = gets
+            case response
+            when /^[yY]/
+              p "再生成"
+              redo
+            when /^[nN]/, /^$/
+              p "n/a"
+              return
+            end
+          end
         end
       end
     end
