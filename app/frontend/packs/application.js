7473f9e805f6160ad3614e31ee5d09e13eeba90c
Yoshito Sato <yoshitosato@YoshitonoMacBook-Pro.local>
2020-02-27 23:47:42 +0900

    vuetify関連を削除

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,22 +4,10 @@ import '../stylesheets/application.css'
 
 // Vue
 import Vue from 'vue'
-
-// Vuetify
-import "vuetify/dist/vuetify.min.css"
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import '@mdi/font/css/materialdesignicons.css'
-import Vuetify from 'vuetify'
-
-// main.js
 import App from '../App.vue'
-
-Vue.use(Vuetify)
-const vuetify = new Vuetify()
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    vuetify,
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
