5500580cb5e099ccaecf70a239775b3b0b463e7f
Yoshito Sato <yoshitosato@YoshitonoMacBook-Pro.local>
2020-02-28 12:12:09 +0900

    プラポリ文言追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def privacy
+  end
+
+  def form
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+
+  # devise
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  # static_pages
   root   'static_pages#home'
+  get   '/privacy', to: 'static_pages#privacy'
+  get   '/form', to: 'static_pages#form'
+
   resources :quiz_wadokaichins, only: [:index, :show]
   post  '/quiz_wadokaichins/:id',  to: 'quiz_wadokaichins#show'
 end
