b88d143fbe57ababfeda60e8abee143cfcc62fdd
ys8906 <58162637+ys8906@users.noreply.github.com>
2020-03-01 00:43:04 +0900

    コンフリクト解消のため、若干branch:indexの方によせた

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#root'
 
   # devise
   devise_for :users, controllers: {
@@ -7,11 +8,10 @@ Rails.application.routes.draw do
   }
 
   # static_pages
-  root   'static_pages#home'
   get   '/privacy', to: 'static_pages#privacy'
   get   '/form', to: 'static_pages#form'
   get   '/form/send_inquiry', to: 'static_pages#send_inquiry'
 
-  resources :quiz_wadokaichins, only: [:index, :show]
+  resources :quiz_wadokaichins, only: [:show]
   post  '/quiz_wadokaichins/:id',  to: 'quiz_wadokaichins#show'
 end
