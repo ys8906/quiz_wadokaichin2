d34090dc8d9423b8c473024ee46481322fbe90b4
ys8906 <58162637+ys8906@users.noreply.github.com>
2020-03-03 01:26:58 +0900

    クイズページに無限ローディング換装

--- app/controllers/quiz_wadokaichins_controller.rb
+++ app/controllers/quiz_wadokaichins_controller.rb
@@ -5,5 +5,6 @@ class QuizWadokaichinsController < ApplicationController
     @reactions  = @quiz.quiz_wadokaichin_reactions
     @remote_ip  = request.remote_ip
     @savedata   = current_user&.quiz_wadokaichin_savedata&.where(quiz_wadokaichin_id: @quiz.id)
+    @related_quizzes    = QuizWadokaichin.page(params[:page]).per(6)
   end
 end
