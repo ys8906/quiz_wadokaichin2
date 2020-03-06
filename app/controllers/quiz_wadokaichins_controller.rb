5a139280f9cc96d639469035534fe6f0dd69ae85
ys8906 <58162637+ys8906@users.noreply.github.com>
2020-03-06 15:26:01 +0900

    show.html.erb: request.ip/request.urlをインスタンス変数から外して、viewで直接取得するように変更

--- app/controllers/quiz_wadokaichins_controller.rb
+++ app/controllers/quiz_wadokaichins_controller.rb
@@ -23,9 +23,7 @@ class QuizWadokaichinsController < ApplicationController
 
   def show
     @quiz       = QuizWadokaichin.find(params[:id])
-    @url        = request.url
     @reactions  = @quiz.quiz_wadokaichin_reactions
-    @remote_ip  = request.remote_ip
     @savedata   = current_user&.quiz_wadokaichin_savedata&.where(quiz_wadokaichin_id: @quiz.id)
     @related_quizzes    = QuizWadokaichin.order("RAND()").limit(9)
   end
