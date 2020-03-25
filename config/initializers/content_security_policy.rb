0b8bfd146fcac865422f6d10df0c7d9615fff487
ys8906 <58162637+ys8906@users.noreply.github.com>
2020-03-25 22:27:28 +0900

    content_security_policy: development以外の環境変更を削除（vueがproductionを動かすため）

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,8 +2,6 @@
 Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval, :unsafe_inline
-  else
-    policy.script_src :self, :https,:unsafe_inline
   end
 end
 
