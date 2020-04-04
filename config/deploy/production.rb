67f11bb5a7a1a128f97425238fb76382171610d4
ys8906 <58162637+ys8906@users.noreply.github.com>
2020-04-05 02:20:00 +0900

    webpackerのコンパイルができるように修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,5 +60,5 @@
 #     # password: "please use keys"
 #   }
 
-server '18.176.129.236', user: 'ys8906', roles: %w{app}
+server '18.176.129.236', user: 'ys8906', roles: %w{app web}
 set :ssh_options, keys: '~/.ssh/quiz_wadokaichin_key_rsa'
