b35bfc1d4fdc7f3eeb3bd3cd756ffe83d13b4401
ys8906 <58162637+ys8906@users.noreply.github.com>
2020-05-02 19:48:36 +0900

    パスワードリセット：メアドが存在しない場合も、同じ画面を表示するように設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # devise
   devise_for :users, controllers: {
     sessions: 'users/sessions',
+    passwords: 'users/passwords',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
