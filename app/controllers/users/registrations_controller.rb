14b90709d036ae5469942ca6df6e97e83e91feed
ys8906 <58162637+ys8906@users.noreply.github.com>
2020-05-06 14:24:11 +0900

    breadcrumbs for account pages

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,9 +6,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    add_breadcrumb 'サインアップ', new_user_registration_path
+    super
+  end
 
   # POST /resource
   # def create
@@ -16,9 +17,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
   # def update
